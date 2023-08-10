69764f551e294e923c38a7798794f77dca4ee741
Doma <domingos.ricardi@gmail.com>
2023-08-10 00:50:42 +0000

    Deploy on Vercel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home(){
-    return <h1>Olá pessoa! Se você está lendo isso é porque eu amo você!!!</h1>
+    return <h1>Olá pessoa! Se você está lendo isso é porque eu amo você!!!
+        <img src="https://usagif.com/wp-content/uploads/gif-heart-47.gif"></img>
+    </h1>        
 }
 
 export default Home;
