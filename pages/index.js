8faeb0495f423721b19ed46f59b256f47fddd966
Doma <domingos.ricardi@gmail.com>
2023-08-21 21:23:34 +0000

    Change Home page message

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Olá pessoa! Se você está lendo isso é porque eu amo você!!!
-      <img src="https://usagif.com/wp-content/uploads/gif-heart-47.gif"></img>
-    </h1>
-  );
+  return <h1>Página em Construção</h1>;
 }
 
 export default Home;
