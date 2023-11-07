20f069297c6e0afa09acca9b8ab438b0e980647c
Doma <domingos.ricardi@gmail.com>
2023-11-07 22:22:28 +0000

    Add enviroment and database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, reponse) {
+import database from "../../../../infra/database.js";
+
+async function status(request, reponse) {
+  const result = await database.query("SELECT 1 + 1 AS SUM;");
+  console.log(result.rows);
+
   reponse
     .status(200)
     .json({ status: 200, mensagem: "Aplicação rodando normalmente" });
