b1f220ac511b6a98ea46eccd09cbc4a6ef08c087
Doma <domingos.ricardi@gmail.com>
2024-02-27 23:37:17 +0000

    Add try/catch on database connection

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,16 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
+  const credentials = {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
+  };
+  const client = new Client(credentials);
+
+  console.log("Credenciais do Postgres: ", credentials);
 
   try {
     await client.connect();
@@ -15,6 +18,7 @@ async function query(queryObject) {
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
