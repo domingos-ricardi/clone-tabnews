d136611521889ef82c45397e02c76ab8bb4ae8bd
Doma <domingos.ricardi@gmail.com>
2024-04-10 14:18:39 +0000

    Adding try/catch on migatrion controller

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -13,23 +13,26 @@ export default async function migrations(request, response) {
     migrationsTable: "pgmigrations",
   };
 
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+  try {
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+      return response.status(200).json(pendingMigrations);
+    }
+
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+        dryRun: false,
+      });
+
+      if (migratedMigrations.length > 0)
+        return response.status(201).json(migratedMigrations);
+      else return response.status(200).json(migratedMigrations);
+    }
+
+    return response.status(405).end();
+  } catch (error) {
+  } finally {
     await dbClient.end();
-    return response.status(200).json(pendingMigrations);
   }
-
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dryRun: false,
-    });
-    await dbClient.end();
-
-    if (migratedMigrations.length > 0)
-      return response.status(201).json(migratedMigrations);
-    else return response.status(200).json(migratedMigrations);
-  }
-
-  return response.status(405).end();
 }
