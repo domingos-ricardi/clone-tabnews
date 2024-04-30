ca622a1362b0c767e72dbb0dcaf5f452359111dd
Doma <domingos.ricardi@gmail.com>
2024-04-30 23:18:54 +0000

    Test error migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -13,27 +13,23 @@ export default async function migrations(request, response) {
     migrationsTable: "pgmigrations",
   };
 
-  try {
-    if (request.method === "GET") {
-      const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
-      return response.status(200).json(pendingMigrations);
-    }
-
-    if (request.method === "POST") {
-      const migratedMigrations = await migrationRunner({
-        ...defaultMigrationsOptions,
-        dryRun: false,
-      });
-
-      if (migratedMigrations.length > 0)
-        return response.status(201).json(migratedMigrations);
-      else return response.status(200).json(migratedMigrations);
-    }
-
-    return response.status(405).end();
-  } catch (error) {
-    return response.status(500).end();
-  } finally {
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
     await dbClient.end();
+    return response.status(200).json(pendingMigrations);
   }
+
+  if (request.method === "POST") {
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dryRun: false,
+    });
+    await dbClient.end();
+
+    if (migratedMigrations.length > 0)
+      return response.status(201).json(migratedMigrations);
+    else return response.status(200).json(migratedMigrations);
+  }
+
+  return response.status(405).end();
 }
