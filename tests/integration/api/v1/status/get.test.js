5c95c0543b1ebc6989134ecfa4d016d2d3e6c4bd
Doma <domingos.ricardi@gmail.com>
2024-09-19 02:59:03 +0000

    test: rename all desciptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,18 +4,25 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET to /api/v1/status", () => {
+  const url = "http://localhost:3000/api/v1/status";
 
-  const responseBody = await response.json();
-  const parseAt = new Date(responseBody.update_at).toISOString();
-  expect(responseBody.update_at).toEqual(parseAt);
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch(url);
 
-  expect(responseBody.dependencies.database).toBeDefined();
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      const responseBody = await response.json();
+      const parseAt = new Date(responseBody.update_at).toISOString();
+
+      expect(response.status).toBe(200);
+      expect(responseBody.update_at).toEqual(parseAt);
+
+      expect(responseBody.dependencies.database).toBeDefined();
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
 
 // eslint-disable-next-line jest/no-commented-out-tests
