58a00ca88ab856c8d80ba52f754676bb33f1b953
Doma <domingos.ricardi@gmail.com>
2025-02-28 23:17:00 +0000

    fix: fix controller and api-errors

--- infra/errors/db-errors.js
+++ infra/errors/db-errors.js
@@ -1,17 +1,1 @@
-export class InternalServerError extends Error {
-  constructor({ cause }) {
-    super("Um erro interno não esperado ocorreu", { cause });
-    this.name = "IternalServerError";
-    this.action = "Entre em contato com o suporte";
-    this.statusCode = cause?.statusCode || 500;
-  }
 
-  toJSON() {
-    return {
-      name: this.name,
-      message: this.message,
-      action: this.action,
-      statusCode: this.statusCode,
-    };
-  }
-}
