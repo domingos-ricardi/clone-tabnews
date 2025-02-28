01a249a73ef18a65a96a8d592c2314696cba5247
Doma <domingos.ricardi@gmail.com>
2025-02-28 23:57:38 +0000

    refactor: update `/api/v1/migratios.js` to use `migrator` model

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,10 +1,6 @@
 import { createRouter } from "next-connect";
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database.js";
 import controller from "infra/controller.js";
-
-let dbClient;
+import migrator from "models/migrator.js";
 
 const router = createRouter();
 router.get(getHandler);
@@ -12,47 +8,15 @@ router.post(postHandler);
 
 export default router.handler(controller.errorHandlers);
 
-function getMigrationsOptions(dbClient, liveRun) {
-  return {
-    dbClient: dbClient,
-    dryRun: !liveRun, //Define o modo de execução: TRUE -> modo de teste - FALSE: modo de produção
-    dir: resolve("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-}
-
-async function getPendingMigrations(method) {
-  dbClient = await database.getNewClient();
-  const defaultMigrationsOptions = getMigrationsOptions(
-    dbClient,
-    method === "POST",
-  );
-
-  return await migrationRunner(defaultMigrationsOptions);
-}
-
 async function getHandler(request, response) {
-  return migrations("GET", response);
+  const pendingMigrations = await migrator.listPendingMigrations();
+  return response.status(200).json(pendingMigrations);
 }
 
 async function postHandler(request, response) {
-  return migrations("POST", response);
-}
-
-async function migrations(method, response) {
   let statusCode = 200;
+  const pendingMigrations = await migrator.runPendingMigrations();
+  if (pendingMigrations.length > 0) statusCode = 201;
 
-  try {
-    const pendingMigrations = await getPendingMigrations(method);
-
-    if (method === "POST") {
-      if (pendingMigrations.length > 0) statusCode = 201;
-    }
-
-    return response.status(statusCode).json(pendingMigrations);
-  } finally {
-    if (dbClient !== undefined) await dbClient.end();
-  }
+  return response.status(statusCode).json(pendingMigrations);
 }
