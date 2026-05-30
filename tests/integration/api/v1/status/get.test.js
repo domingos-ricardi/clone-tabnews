189de660ddc6e9b0c9cc6125445a6d6fb9fb641b
Doma <domingos.ricardi@gmail.com>
2026-05-30 22:24:30 +0000

    test: add default user coveraga for Get Status

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -27,6 +27,32 @@ describe("GET to /api/v1/status", () => {
     });
   });
 
+  describe("Default user", () => {
+    test("Retrieving current system status", async () => {
+      const defaultUser = await orchestrator.createUser({});
+      await orchestrator.activateUser(defaultUser);
+      await orchestrator.addFeaturesToUser(defaultUser, ["read:status"]);
+      const sessionObject = await orchestrator.createSession(defaultUser);
+
+      const response = await fetch(url, {
+        headers: {
+          cookie: `session_id=${sessionObject.token}`,
+        },
+      });
+
+      const responseBody = await response.json();
+      const parseAt = new Date(responseBody.update_at).toISOString();
+
+      expect(response.status).toBe(200);
+      expect(responseBody.update_at).toEqual(parseAt);
+
+      expect(responseBody.dependencies.database).toBeDefined();
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      expect(responseBody.dependencies.database).not.toHaveProperty("version");
+    });
+  });
+
   describe("Privileged user", () => {
     test("Retrieving current system status", async () => {
       const privilegedUser = await orchestrator.createUser({});
